755da3f5dbb0e8ef32b7b5bf443f801e4ed1fd1b
OM <omyengantiwar26@gmail.com>
2026-03-05 01:18:59 +0530

    Enhance dice game with roll count tracking
    Added functionality to track the number of rolls and display it after each attempt.

--- dice.py
+++ dice.py
@@ -4,19 +4,26 @@ print("Welcome to Dice Roll Game")
 
 score = 0
 
+No_times = 0
+
 while True:
 
     user = input("Enter S to start the game or Q to exit the game: ").lower().strip()
 
     if user == "s":
         dice = random.randint(1,6)
-        print("You rolled:", dice)
+        print("You rolled:")
 
         if dice == 6:
             print("u got 6. +1 point")
             score += 1
+            No_times += 1
+            print("number of times rolling",No_times)
+            break
         else:
             print("u did not get 6, try again")
+            No_times += 1
+            print("number of times rolling",No_times)
 
         print("ur total score:", score)
 
